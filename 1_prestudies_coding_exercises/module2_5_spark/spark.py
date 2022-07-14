a39e662a45a191fb2bb156d744256bb3b52686c5
orosz-attila <orosz.attila.mail@gmail.com>
2022-07-14 20:50:54 +0200

    prestudies spark

---
+++ 1_prestudies_coding_exercises/module2_5_spark/spark.py
@@ -0,0 +1,13 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import SQLContext 
+
+if __name__ == '__main__':
+    scSpark = SparkSession \
+        .builder \
+        .appName("reading csv") \
+        .getOrCreate()
+
+data_file = 'data*.csv'
+sdfData = scSpark.read.csv(data_file, header=True, sep=",").cache()
+print('Total Records = {}'.format(sdfData.count()))
+sdfData.show()
